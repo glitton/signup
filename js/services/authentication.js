fa3caf68c929ad7ac8e4d9447f93a215ec625448
Generosa Litton <generosa.litton@yahoo.com>
2017-10-11 19:28:59 -0700

    added access to firebase authentication

--- js/services/authentication.js
+++ js/services/authentication.js
@@ -1,22 +1,49 @@
-myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', function($rootScope, $firebaseAuth) {
+myApp.factory('Authentication', ['$rootScope', '$location', '$firebaseObject', '$firebaseAuth', function($rootScope, $location, $firebaseObject, $firebaseAuth) {
 
   // Create a variable to reference firebase DB
   var ref = firebase.database().ref();
   // Create variable to reference firebase authentication
   var auth = $firebaseAuth();
 
+  // Method that detects when a user is logged userInfo
+  auth.$onAuthStateChanged(function(authUser) {
+    if(authUser) {
+      var userRef = ref.child('users').child(authUser.uid);
+      var userObj = $firebaseObject(userRef);
+      $rootScope.currentUser = userObj;
+    } else {
+      $rootScope.currentUser = '';
+    }
+  })
+
+
   return {
     login: function(user) {
-      $$rootScope.message = "Welcome " + $$rootScope.user.email;
+      auth.$signInWithEmailAndPassword(
+        user.email,
+        user.password
+      ).then(function(user){
+        $location.path('/success')
+      }).catch(function(error){
+        $rootScope.message = error.message;
+      }); //signInwithEmailAndPassword
     }, //login
     signup: function(user) {
       auth.$createUserWithEmailAndPassword(
         user.email, 
         user.password
     ).then(function(regUser) {
-        $rootScope.message = "Hello " + user.firstname + ". You now have access to magic!"; 
+      var regRef = ref.child('users')
+        .child(regUser.uid).set({
+          date: firebase.database.ServerValue.TIMESTAMP,
+          regUser: regUser.uid,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          email: user.email
+        }) // userInfo
+      $rootScope.message = "Hello " + user.firstname + ". You now have access to magic!"; 
     }).catch(function(error) {
-      $$rootScope.message = error.message;
+      $rootScope.message = error.message;
     }); //end of $createUserWithEmailAndPassword
     } //signup
   }; //return
